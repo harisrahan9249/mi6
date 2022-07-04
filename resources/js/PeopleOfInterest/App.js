fd144831aa516c02434f37b97f5080eaaccb9313
OlhaRohoza <olha.rohoza@gmail.com>
2022-07-04 16:44:49 +0200

    add api to react part

--- resources/js/PeopleOfInterest/App.js
+++ resources/js/PeopleOfInterest/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        const response = await fetch('api/people-of-interest');
+        const parsedRespose = await response.json();
+        console.log(parsedRespose);
+        setData(parsedRespose);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
-        <div className="welcome">
-            <h1>Welcome to REACT (!!!) People of Interest</h1>
-        </div>
+        data == null
+            ? <h1>Loading</h1>
+            : <div className="">
+                {data.map((person, index) => (
+                    <><p key={index}>{person.name} is {person.occupation}</p>
+                        <p>Known Aliases: </p>
+                        <ul>
+                            {person.aliases.map((alias, i) => (
+                                <li key={i}>{alias.alias}</li>))}
+                        </ul>
+                    </>
+                ))}
+            </div>
     )
 }
