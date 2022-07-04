f1a6f41a9272b26c99d022d8390d17a8e7c7dbcf
OlhaRohoza <olha.rohoza@gmail.com>
2022-07-04 14:38:50 +0200

    add layout and 2 pages

--- resources/views/people-of-interest/index.blade.php
+++ resources/views/people-of-interest/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="welcome">
-            <h1>Welcome to MI6!</h1>
-        </div>
+        <h1>Welcome to People of Interest</h1>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/people-of-interest', function () {
+    return view('people-of-interest.index');
+});
